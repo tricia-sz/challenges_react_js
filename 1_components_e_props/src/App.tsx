9f060306e51ed77d26c57ca71298700c19debe9c
Patrícia Souza <patricia.sz.br@gmail.com>
2023-09-27 14:55:00 -0300

    Challenge 5 - Component Class

--- 1_components_e_props/src/App.tsx
+++ 1_components_e_props/src/App.tsx
@@ -3,6 +3,7 @@ import Family from "./components/Family";
 import { HelloName } from "./components/HelloName";
 import { HelloWorld } from "./components/HelloWorld";
 import Profile from "./components/Profile";
+import ProfileClass from "./components/ProfileClass";
 
 
 function App() {
@@ -22,6 +23,9 @@ function App() {
       <Profile name="João" age={30} job="Programador" />
       <h2>4 - Composição de componentes</h2>
       <Family members={members} />
+      <h2>5 - Componente de Classe</h2>
+      <ProfileClass name={"Rodrigo"} age={45} job={"Lavrador"} />
+
     </>
   );
 }
