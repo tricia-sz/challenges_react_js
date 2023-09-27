e772ac7d0772c33f591e68379c7e02878da7ec45
Patrícia Souza <patricia.sz.br@gmail.com>
2023-09-27 12:32:17 -0300

    Challenge 1 - First Component

---
+++ 1_components_e_props/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { HelloWorld } from './components/HelloWorld'
+
+function App() {
+  return (
+   <>
+    <h2>1 - Criando um component Simples</h2>
+    <HelloWorld />
+   </>
+  )
+}
+
+export default App
